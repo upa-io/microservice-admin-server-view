06b8946cacfcc8b1faef8673ebab6b024d91a4fa
Jesus Aguirre <info.aguirre.jesus@gmail.com>
2022-08-26 21:04:01 -0500

    Base de repositorio

---
+++ src/main/java/com/test/springboot/admin/server/SpringbootAdminServerApplication.java
@@ -0,0 +1,19 @@
+package com.test.springboot.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+@Slf4j
+public class SpringbootAdminServerApplication {
+
+	public static void main(String[] args) {
+		log.info("Inciando aplicacion Microservice-admin-server-view");
+
+		SpringApplication.run(SpringbootAdminServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springboot/admin/server/SpringbootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springboot.admin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
